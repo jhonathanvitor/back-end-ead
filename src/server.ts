a5989405fb9011f4f6ab641e66dbafe679edb2de
jhonathan vitor <jhonathanvitor@gmail.com>
2023-05-30 15:28:39 -0400

    created docker-compose

--- src/server.ts
+++ src/server.ts
@@ -12,4 +12,4 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.listen(8080, () => console.log("🚀 Server is running!"));
+app.listen(3333, () => console.log("🚀 Server is running!"));
