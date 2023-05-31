f6d03e713609e93f7f1721e9f0d36d0914eccef7
jhonathan vitor <jhonathanvitor@gmail.com>
2023-05-31 18:14:08 -0400

    Entities

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,11 +1,18 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
-import { Entity } from "typeorm";
 
 @Entity("categories")
 class Category {
+    @PrimaryColumn()
     id?: string;
+
+    @Column()
     name: string;
+
+    @Column()
     description: string;
+
+    @CreateDateColumn()
     created_at: Date;
 
     constructor() {
